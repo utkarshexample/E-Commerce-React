017dc6ec381330bb57495004aea213b77d6270aa
Utkarsh Aman <utkasrh.example@gmail.com>
2024-09-12 22:29:34 -0400

    docker changes

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/E-Commerce-React/", // Your GitHub repo name
-  build: {
-    outDir: "dist", // Output directory for the build
-    rollupOptions: {
-      output: {
-        // Ensure module output is compatible for GitHub Pages
-        format: "es",
-      },
-    },
-  },
 });
