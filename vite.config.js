045c5f27415ba4fad77170d88b73a610ef01ac9a
Utkarsh Aman <utkasrh.example@gmail.com>
2024-09-12 22:13:51 -0400

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/E-Commerce-React/", // Your GitHub repo name
   build: {
-    outDir: "dist",
+    outDir: "dist", // Output directory for the build
+    rollupOptions: {
+      output: {
+        // Ensure module output is compatible for GitHub Pages
+        format: "es",
+      },
+    },
   },
-  base: "/E-Commerce-React/",
 });
